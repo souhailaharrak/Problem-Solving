c2dd1afe011d935933ed4495dbf8d0fc694af728
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2022-12-09 00:03:51 +0000

    Find The Sum between 1 to N

---
+++ Somme.js
@@ -0,0 +1,15 @@
+
+//write function that take n as parametre and sum from 1 to n
+// write function that sum all number between 1 to n
+
+function Sum(n){
+  
+    let somme =0;
+      for(let i=1;i<=n;i++){
+
+           somme  += i;
+
+          console.log(somme)
+      }
+}
+Sum(5)
