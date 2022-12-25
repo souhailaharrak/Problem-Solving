2ac2c14e721417d05e4faa33f74b77148f55f115
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2022-12-25 01:28:53 +0100

    Update typeNumber.js

--- typeNumber.js
+++ typeNumber.js
@@ -29,7 +29,9 @@ var readNumber=function(){
       }
   }
    
-   
+   //Is enum type?
+//An enum type is a special data type that enables for a variable to be a set of predefined constants.
+   //The variable must be equal to one of the values that have been predefined for it.
    
    
    
